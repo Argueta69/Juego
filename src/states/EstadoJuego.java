14f40ae5f25e95bd0fe4d7ac539e0c32188fa268
Dani <daniblazquez4b@gmail.com>
2022-03-15 18:36:58 +0100

    Daniel 18:36 15/03/2022 Modificaciones-->Nueva clase para el estado de juego

---
+++ src/states/EstadoJuego.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package states;
+
+/**
+ *
+ * @author FA506IV
+ */
+public class EstadoJuego {
+    
+    private String nombre;
+    private int puntos;
+    private int nivel;
+    private int vidas;
+
+    public EstadoJuego() {
+    }
+
+    public EstadoJuego( String nombre,  int puntos, int nivel, int vidas) {
+      
+        this.nombre = nombre;
+        this.puntos = puntos;
+        this.nivel = nivel;
+        this.vidas = vidas;
+    }
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+    @Override
+    public String toString() {
+        return "EstadoJuego{"  + ", nombre=" + nombre + ", puntos=" + puntos + ", nivel=" + nivel + ", vidas=" + vidas + '}';
+    }
+    
+    
+            
+}
